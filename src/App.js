29f923e471d3bded944688b081de4f87115df6f0
Micah Peterson <micahpeterson@live.com>
2020-08-14 11:28:09 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,97 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import {Row,Col,Card, Container} from 'react-bootstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      regions: [],
+      searchResults: ""
+    }
+  }
+
+  componentDidMount = async () => {
+
+    let url = "https://corona.lmao.ninja/v2/states";
+
+    let response = await fetch(url);
+    let results  = await response.json();
+
+    console.log(results)
+
+    this.setState({
+      regions: results
+    })
+  }
+
+  handleChange = (e) => {
+
+    this.setState({
+      searchResults: e.target.value
+    })
+  }
+
+  render() {
+
+    let filtered = this.state.regions.filter(region => {
+
+      // filter out news articles based on what user is typing in input field
+      return region.state.toLowerCase().includes(this.state.searchResults.toLowerCase());
+      
+    });
+
+    let regions = filtered.map(region => {
+
+      return <Col className="col-4">
+                <Card className="mb-3">
+                <Card.Body>
+                <Card.Title>{region.state}</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted">{region.cases}</Card.Subtitle>
+                <Card.Text>
+
+                        <p>
+                            Today's cases: {region.todayCases} <br/>
+                            Today's deaths: {region.todayDeaths} <br/>
+                            Total deaths: {region.deaths} <br/>
+                        </p>
+                  
+                </Card.Text>
+                <Card.Link target="_blank" href="https://corona.lmao.ninja/v2/states">Source</Card.Link>
+              </Card.Body>
+              </Card>
+            </Col>
+
+    })
+
+    return (
+      <>
+
+        <Container>
+
+          <Row className="justify-content-center mt-5 mb-2">
+            <h2>Search coronavirus data by state</h2>
+          </Row>
+
+          <Row className="justify-content-center mb-3">
+            <input type="text" onChange={this.handleChange}/>
+          </Row>
+
+          <Row className="justify-content-center mb-3">
+            Filtering cards to ones that include: {this.state.searchResults}
+          </Row>
+
+          <Row>
+            {regions}
+          </Row>
+
+        </Container>
+
+      </>
+    )
+  }
 }
 
-export default App;
+export default App
+
